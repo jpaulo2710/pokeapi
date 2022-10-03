be71f6f52cae98a2ff9d7e715dabb8b42c0f7030
joun <joao.gutstein@gft.com>
2022-10-03 11:29:28 -0300

    Commit inicial

---
+++ README.md
@@ -0,0 +1,8 @@
+### 🛠 Ferramentas:
+
+- VSCode
+
+
+### 🎯 Objetivos do projeto:
+
+- Construir um pequeno sistema (API REST) para consumo de dados da pokeapi.co.
---
+++ script.js
@@ -0,0 +1,52 @@
+var formulario = document.querySelector('form')
+
+formulario.addEventListener('submit', function(e){
+    //Bloqueia o refresh da pagina
+    e.preventDefault()
+
+    //Url da pesquisa
+    let urlForm = "https://pokeapi.co/api/v2/pokemon/";
+
+    //Valor do input name
+    let nome = document.getElementById("name")
+
+    //Concatena a url com o inputname
+    urlForm = urlForm + this.name.value
+
+    //Transforma os valores em minusculos
+    urlForm = urlForm.toLocaleLowerCase()
+
+    // ID content
+
+    let resposta = document.getElementById('content')
+
+    //Id ImgPokemon
+
+    let imagem = document.getElementById('imgPokemon')
+
+    //Resposta em HTML
+    let html = ''
+
+    fetch(urlForm)
+        .then(resposta => resposta.json())
+        .then(function(data){
+            console.log(data)
+            html = 'Nome: ' + maiuscula(data.name)  + '<br>'
+            html = html + 'Type: ' + maiuscula(data.types[0].type.name)
+            resposta.innerHTML = html
+
+            imagem.innerHTML = "<img src='" + data.sprites.front_default + "'><img src=' " + data.sprites.back_default + "'>"
+        })
+        .catch(function(err){
+            if(err == 'SyntaxError: Unexpected token N in JSON at position 0' ){
+                html = 'Pokémon não encontrado! 😒'
+            }else{
+                html = 'Erro:' + err
+            }
+            resposta.innerHTML = html
+        })
+
+});
+function maiuscula(val) {
+    return val[0].toUpperCase() + val.substr(1)
+}
